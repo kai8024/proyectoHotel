cf30d6623b17f2670db1e8422b83e67d10f01311
Jean Paul Lavado <jlavadom@ucentral.edu.co>
2024-10-22 18:50:40 -0500

    octubre

---
+++ src/test/java/com/edu/ucentral/apphotel/ApphotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ucentral.apphotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApphotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
